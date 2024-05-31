73991f7550df6026edc29528dbbf6c62cc23b8ed
hbarquanta <62696594+hbarquanta@users.noreply.github.com>
2024-05-31 19:04:49 +0200

    DP animation app

--- Double_Pendulum_App.py
+++ Double_Pendulum_App.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import streamlit as st
+from matplotlib.animation import FuncAnimation, PillowWriter
+from IPython.display import HTML
 
 # Constants
 g = 9.81  # Acceleration due to gravity (m/s^2)
@@ -40,6 +42,38 @@ def plot_double_pendulum(solution, L1, L2):
     ax.grid(True, linestyle='--', alpha=0.7)
     st.pyplot(fig)
 
+def animate_double_pendulum(solution, L1, L2):
+    theta1, theta2 = solution.y[0], solution.y[2]
+    x1 = L1 * np.sin(theta1)
+    y1 = -L1 * np.cos(theta1)
+    x2 = x1 + L2 * np.sin(theta2)
+    y2 = y1 - L2 * np.cos(theta2)
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+    ax.grid(True, linestyle='--', alpha=0.7)
+    line, = ax.plot([], [], 'o-', lw=2, color='black')
+    trace1, = ax.plot([], [], 'r-', lw=1, alpha=0.5)
+    trace2, = ax.plot([], [], 'b-', lw=1, alpha=0.5)
+    
+    def init():
+        line.set_data([], [])
+        trace1.set_data([], [])
+        trace2.set_data([], [])
+        return line, trace1, trace2
+    
+    def update(frame):
+        thisx = [0, x1[frame], x2[frame]]
+        thisy = [0, y1[frame], y2[frame]]
+        line.set_data(thisx, thisy)
+        trace1.set_data(x1[:frame], y1[:frame])
+        trace2.set_data(x2[:frame], y2[:frame])
+        return line, trace1, trace2
+    
+    ani = FuncAnimation(fig, update, frames=range(len(x1)), init_func=init, blit=True, interval=20)
+    return ani
+
 # Streamlit App
 st.title('Double Pendulum Simulation')
 
@@ -57,3 +91,6 @@ if st.button('Simulate'):
     
     solution = simulate_double_pendulum(y0, t_span, t_eval, L1, L2, m1, m2)
     plot_double_pendulum(solution, L1, L2)
+    ani = animate_double_pendulum(solution, L1, L2)
+    ani_html = ani.to_jshtml()
+    st.components.v1.html(ani_html, height=700)
